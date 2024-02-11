a600ec5e65afefe298191810a14e8faab8e7fe8a
BugbearR <bugbearr@bugbearr.jp>
2024-02-11 06:04:04 +0000

    Add escape sequence examples to string_literal module

---
+++ 0210_string_literal/escape_sequence.py
@@ -0,0 +1,41 @@
+# https://docs.python.org/ja/3/reference/lexical_analysis.html#escape-sequences
+
+# continue line by backslash
+print("abc\
+def")
+
+# backslash
+print("\\")
+
+# single quote
+print("\'")
+
+# double quote
+print("\"")
+
+# bell
+print("\a")
+
+# backspace
+print("abc\bdef")
+
+# form feed
+print("abc\fdef")
+
+# line feed
+print("abc\ndef")
+
+# carriage return
+print("abc\rdef")
+
+# horizontal tab
+print("abc\tdef")
+
+# vertical tab
+print("abc\vdef")
+
+# octal value
+print("\101")
+
+# hex value
+print("\x41")
